87b89d18771c147a486ed6c87d0561669d0a5770
Guillermo López <guillermo.lopez@outlook.com>
2026-07-28 06:55:51 +0200

    Improve

--- src/System.Management.Automation/engine/Modules/PathHandling.cs
+++ src/System.Management.Automation/engine/Modules/PathHandling.cs
@@ -5,9 +5,15 @@ namespace System.Management.Automation.Internal
 {
     internal static class PathHandling
     {
+        /// <summary>
+        /// Normalizess <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">The path to normalize directory separators for.</param>
+        /// <returns>A path based on <paramref name="path"/> that contains only default
+        /// directory separators and does not end with one.</returns>
         public static string NormalizeDirectorySeparators(string path)
         {
-            return path.Replace(StringLiterals.AlternatePathSeparator, StringLiterals.DefaultPathSeparator);
+            return path.Replace(StringLiterals.AlternatePathSeparator, StringLiterals.DefaultPathSeparator).TrimEnd(StringLiterals.DefaultPathSeparator);
         }
     }
 }
